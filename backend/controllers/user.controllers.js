9d11354de36d671023a36efbf5d99896ebfeea80
klvallejo1 <leandrohernandez1903@gmail.com>
2024-07-04 23:10:54 -0500

    Metodos de login con verificacion de token correctos

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,28 +1,12 @@
 const User = require('../models/user.js');
-const express = require('express');
-const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const userController = {};
 
 // Controlador para obtener todos los usuarios
 userController.getUsers = async (req, res) => {
     res.send('Lista de usuarios');
-};
-
-//Controlador para agregar un usuario
-userController.addUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const newUser = new User({ email, password });
-        await newUser.save();
-        const token = jwt.sign({ _id: newUser._id }, 'secretKey');
-        res.status(200).json({ token });
-        console.log(newUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error al registrar el usuario');
-    }
 };
 
 // Controlador para ingreso
@@ -35,7 +19,9 @@ userController.loginUser = async (req, res) => {
         if (!user) {
             return res.status(401).send("El correo no existe");
         }
-        if (user.password !== password) {
+        
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+        if (!isPasswordValid) {
             return res.status(401).send("Clave incorrecta");
         }
 
@@ -45,6 +31,33 @@ userController.loginUser = async (req, res) => {
         console.error(error);
         res.status(500).send('Error en el ingreso');
     }
+};
+
+// Controlador para registro
+userController.registerUser = async (req, res) => {
+    try {
+        req.body.password = bcrypt.hashSync(req.body.password, 5);
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+};
+
+// Controlador para ingreso (login)
+userController.loginUserSimple = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return res.json({ error: 'Error en el usuario o contraseña' });
+    }
+
+    const eq = bcrypt.compareSync(req.body.password, user.password);
+    if (!eq) {
+        return res.json({ error: 'Error en el usuario o contraseña' });
+    }
+
+    res.json({ success: 'Login correcto!' });
 };
 
 userController.getTasks = async (req, res) => {
@@ -65,16 +78,14 @@ userController.getTasks = async (req, res) => {
             descripcion: 'Informacion tarea3'
         }
     ])
-}
+};
 
 function verificarToken(req, res, next) {
-
     console.log(req.headers.authorization);
     if (!req.headers.authorization) {
         return res.status(401).send("No tiene autorización para continuar");
     }
-    const
-        token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.authorization.split(' ')[1];
     if (token == 'null') {
         return res.status(401).send("No existe token");
     }
--- backend/routes/server-routes.js
+++ backend/routes/server-routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const user = require('../controllers/user.controllers');
 const userController = require('../controllers/user.controllers');
 
-router.get('/', user.getUsers);
-router.post('/registro', user.addUser);
+router.get('/', userController.getUsers);
 router.post('/ingreso', userController.loginUser);
 router.get('/tareas', userController.getTasks);
- 
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUserSimple);
+
 module.exports = router;
